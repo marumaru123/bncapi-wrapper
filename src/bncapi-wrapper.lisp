451e024a51bd1c83ef220b06ea642be760d876c3
marumaru123 <include321321321@gmail.com>
2020-08-21 16:37:24 +0900

    a

--- src/bncapi-wrapper.lisp
+++ src/bncapi-wrapper.lisp
@@ -4,7 +4,7 @@
         :drakma
         :ironclad
         :cl-json)
-  (:export :order-book :new-order :trade-fee))
+  (:export :order-book :new-order :cancel-order :query-order :trade-fee))
 (in-package :bncapi-wrapper)
 
 (defparameter *endpoint-url*     "https://api.binance.com")
@@ -71,6 +71,20 @@
                          :content             body
                          :additional-headers  extra-headers)))
 
+(defun delete-private-api (key path body)
+  (let* ((drakma:*drakma-default-external-format* :utf-8)
+	 (drakma:*text-content-types* '(("application" . "json")))
+	 (url           (concatenate 'string *endpoint-url* path))
+	 (extra-headers (create-extra-headers key)))
+    (drakma:http-request url
+                         :user-agent          :explorer
+	                 :method              :delete
+	                 :content-type        *api-content-type*
+	                 :external-format-out :utf-8
+                         :external-format-in  :utf-8
+                         :content             body
+                         :additional-headers  extra-headers)))
+
 (defun order-book (symbol)
   (let ((path (concatenate 'string "/api/v3/depth?symbol=" symbol)))
     (get-public-api path)))
@@ -80,8 +94,22 @@
          (query-string (concatenate 'string "symbol=" symbol "&side=" side "&type=" type "&timeInForce=" timeInForce "&quantity=" (princ-to-string quantity) "&price=" (princ-to-string price) "&recvWindow=" (princ-to-string recvWindow) "&timestamp=" (princ-to-string timestamp)))
 	 (signature (hex (hmac_sha256 secret query-string)))
 	 (body      (concatenate 'string query-string "&signature=" signature)))
-    (print body)
     (post-private-api key "/api/v3/order" body)))
+
+(defun cancel-order (key secret symbol order-id &optional (recvWindow 5000))
+  (let* ((timestamp    (get-timestamp))
+         (query-string (concatenate 'string "symbol=" symbol "&orderId=" (princ-to-string order-id) "&recvWindow=" (princ-to-string recvWindow) "&timestamp=" (princ-to-string timestamp)))
+	 (signature (hex (hmac_sha256 secret query-string)))
+	 (body      (concatenate 'string query-string "&signature=" signature)))
+    (delete-private-api key "/api/v3/order" body)))
+
+(defun query-order (key secret symbol order-id &optional (recvWindow 5000))
+  (let* ((timestamp    (get-timestamp))
+         (query-string (concatenate 'string "symbol=" symbol "&orderId=" (princ-to-string order-id) "&recvWindow=" (princ-to-string recvWindow) "&timestamp=" (princ-to-string timestamp)))
+	 (signature (hex (hmac_sha256 secret query-string)))
+	 (parameter (concatenate 'string "?" query-string "&signature=" signature))
+	 (url       (concatenate 'string "/api/v3/order" parameter)))
+    (get-private-api key url)))
 
 (defun trade-fee (key secret)
   (let* ((timestamp    (get-timestamp))
@@ -89,4 +117,3 @@
 	 (signature (hex (hmac_sha256 secret query-string)))
 	 (path      (concatenate 'string "/wapi/v3/tradeFee.html?" query-string "&signature=" signature)))
     (get-private-api key path)))
-
